96b83538b37f27008e37022a670d297240578194
Akseli Palén <akseli.palen@gmail.com>
2019-12-06 00:34:35 +0200

    Move create-channel and remove-channel to higher reducer

--- src/timeline/channelTitles/reduce.js
+++ src/timeline/channelTitles/reduce.js
@@ -8,32 +8,6 @@ module.exports = (model, ev) => {
       })
     }
 
-    case 'CREATE_CHANNEL': {
-      const sizeReference = way.channel(model.timeline, 0)
-      const newChannel = way.fill(sizeReference, null)
-      return Object.assign({}, model, {
-        timeline: way.mix(newChannel, model.timeline),
-        channels: [].concat([{
-          title: '?',
-          backgroundColor: '#8A1C82'
-        }], model.channels),
-        channelOnEdit: 0 // Open name edit input
-      })
-    }
-
-    case 'CREATE_FRAME': {
-      const sizeReference = way.frame(model.timeline, 0)
-      const newFrame = way.fill(sizeReference, null)
-      return Object.assign({}, model, {
-        timeline: way.join(model.timeline, newFrame),
-        frames: [].concat(model.frames, [{
-          title: '?'
-        }]),
-        // Open name edit input
-        frameOnEdit: way.len(model.timeline)
-      })
-    }
-
     default:
       return model
   }
--- src/timeline/reduce.js
+++ src/timeline/reduce.js
@@ -9,6 +9,36 @@ module.exports = (state, ev) => {
       })
     }
 
+    case 'CREATE_CHANNEL': {
+      ev.
+      return Object.assign({}, state, {
+        timeline: Object.assign({}, state.timeline, {
+          meta: Object.assign({}, state.timeline.meta, {
+            channelOrder: state.timeline.meta.channelOrder
+            channels:
+          })
+        }),
+        channels: [].concat([{
+          title: '?',
+          backgroundColor: '#8A1C82'
+        }], model.channels),
+        channelOnEdit: 0 // Open name edit input
+      })
+    }
+
+    case 'CREATE_FRAME': {
+      const sizeReference = way.frame(model.timeline, 0)
+      const newFrame = way.fill(sizeReference, null)
+      return Object.assign({}, model, {
+        timeline: way.join(model.timeline, newFrame),
+        frames: [].concat(model.frames, [{
+          title: '?'
+        }]),
+        // Open name edit input
+        frameOnEdit: way.len(model.timeline)
+      })
+    }
+
     default:
       return state
   }
