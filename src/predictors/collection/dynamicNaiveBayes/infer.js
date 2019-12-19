ec693b1322001953c2b15c7570479e49afadf2c2
Akseli Palen <akseli.palen@gmail.com>
2019-12-20 01:54:01 +0200

    Correct and convert naiveBayes to use traditional probabilities

--- src/predictors/collection/dynamicNaiveBayes/infer.js
+++ src/predictors/collection/dynamicNaiveBayes/infer.js
@@ -4,47 +4,42 @@ const lib = require('./lib')
 module.exports = (model, cell, memory) => {
   const posField = model.fields[cell.channel].posField
   const negField = model.fields[cell.channel].negField
-  const posPrior = model.priors[cell.channel]
-  const negPrior = -posPrior
+  const posPrior = (1 + model.priors[cell.channel]) / 2
+  const negPrior = 1 - posPrior
 
   const ctxBegin = lib.getBegin(model, cell.time)
   const ctxEnd = lib.getEnd(model, cell.time)
   const context = way.slice(memory, ctxBegin, ctxEnd)
 
-  const posLikelihoodFactors = way.map(posField, (cond, c, t) => {
+  const fac = (avg, c, t) => {
     const ctx = context[c][t]
-    return cond * ctx
-  })
-  const negLikelihoodFactors = way.map(negField, (cond, c, t) => {
-    const ctx = context[c][t]
-    return cond * ctx
-  })
+    const p = (1 + (ctx > 0 ? avg : -avg)) / 2
+    const m = Math.abs(ctx)
+    return Math.pow(p, m)
+  }
+
+  const posLikelihoodFactors = way.map(posField, fac)
+  const negLikelihoodFactors = way.map(negField, fac)
 
   // Likelihoods
-  const posLike = way.reduce(posLikelihoodFactors, (acc, q) => {
-    return (acc * q + acc + q - 1) / 2
-  }, 1)
-  const negLike = way.reduce(negLikelihoodFactors, (acc, q) => {
-    return (acc * q + acc + q - 1) / 2
-  }, 1)
+  const posLike = way.reduce(posLikelihoodFactors, (acc, q) => acc * q, 1)
+  const negLike = way.reduce(negLikelihoodFactors, (acc, q) => acc * q, 1)
 
   // P2(hood|tgt) * P2(tgt)
-  const posSupport = (posLike * posPrior + posLike + posPrior - 1) / 2
-  const negSupport = (negLike * negPrior + negLike + negPrior - 1) / 2
+  const posSupport = posLike * posPrior
+  const negSupport = negLike * negPrior
 
-  // Normalise ternary supports so that their sum becomes 0.
-  // See notes 2019-12-19
   let prediction, posProb, negProb
-  if (posSupport === -1 && negSupport === -1) {
+  if (posSupport === 0 && negSupport === 0) {
     // Undecidable
-    posProb = 0
-    negProb = 0
+    posProb = 0.5
+    negProb = 0.5
     prediction = 0
   } else {
-    const s = 2 / (posSupport + negSupport + 2)
-    posProb = s * posSupport + s - 1
-    negProb = s * negSupport + s - 1
-    prediction = posProb
+    // Normalise supports so that their sum becomes 1.
+    posProb = posSupport / (posSupport + negSupport)
+    negProb = negSupport / (posSupport + negSupport)
+    prediction = 2 * posProb - 1 // ternary
   }
 
   // Maximum a posteriori decision
