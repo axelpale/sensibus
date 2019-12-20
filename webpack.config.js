a0046716485cdf26c5a68a683e2e7d2062c82eb8
Akseli Palén <akseli.palen@gmail.com>
2019-12-21 01:17:26 +0200

    Move timeline styles under timeline submodule

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader?url=false'],
       },
     ],
   },
