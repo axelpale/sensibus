51d809d128e1b411b60381595f46286229d0fe36
Akseli Palén <akseli.palen@gmail.com>
2019-12-21 06:00:35 +0200

    Make inspector more human-friendly

--- src/predictors/collection/dynamicNaiveBayes/cellInspector/render.js
+++ src/predictors/collection/dynamicNaiveBayes/cellInspector/render.js
@@ -20,13 +20,16 @@ module.exports = (state, model, dispatch) => {
     return root
   }
 
+  const c = state.timeline.select.channel
+  const t = state.timeline.select.frame
+  const channelTitle = state.timeline.channels[c].title
+  const emptyField = way.fill(model.fields[0].posField, 0) // TODO use mod para
+  const selected = way.set(emptyField, c, -model.fieldOffset, 1)
+
   let innerHTML = ''
 
   // Get prediction data for the selected cell.
   const cellResult = (() => {
-    const select = state.timeline.select
-    const c = select.channel
-    const t = select.frame
     return model.cellResults.find(cellResult => {
       return cellResult.cell.channel === c && cellResult.cell.time === t
     })
@@ -34,29 +37,39 @@ module.exports = (state, model, dispatch) => {
 
   // General info that does not depend if the selected cell is unknown or not.
   innerHTML += generalTemplate({
+    channelTitle: channelTitle,
     c: state.timeline.select.channel,
     t: state.timeline.select.frame,
     memory: state.timeline.way,
-    predictedValue: cellResult ? cellResult.prediction.toFixed(2) : 'N/A'
+    predictedValue: cellResult ? cellResult.prediction.toFixed(2) : 'N/A',
+    posField: renderWay(model.fields[c].posField, {
+      heading: 'If ' + channelTitle + ' then',
+      caption: 'in general.',
+      selected: selected
+    }),
+    negField: renderWay(model.fields[c].negField, {
+      heading: 'If not ' + channelTitle + ' then',
+      caption: 'in general.',
+      selected: selected
+    })
   })
 
   if (cellResult) {
     // A unknown cell is selected. Show how we predict its value.
-    const c = cellResult.cell.channel
     const ctx = cellResult.context
     const selected = way.set(way.fill(ctx, 0), c, -model.fieldOffset, 1)
 
     innerHTML += template({
       contextHtml: renderWay(ctx, {
-        heading: 'Context',
+        heading: 'Selection context',
         selected: selected
       }),
       posFactorsHtml: renderWay(cellResult.posFactors, {
-        heading: 'Likelihood Factors for Positive',
+        heading: 'Likelihood factors for positive',
         selected: selected
       }),
       negFactorsHtml: renderWay(cellResult.negFactors, {
-        heading: 'Likelihood Factors for Negative',
+        heading: 'Likelihood factors for negative',
         selected: selected
       }),
       posSupport: cellResult.posSupport,
