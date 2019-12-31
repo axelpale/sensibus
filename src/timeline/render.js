2de8456851db6265a6477bde3023fe60f40b471b
Akseli Palen <akseli.palen@gmail.com>
2019-12-31 02:16:59 +0200

    observer and renderCanvas

--- src/timeline/render.js
+++ src/timeline/render.js
@@ -3,22 +3,14 @@ const way = require('senseway')
 const memoryViewer = require('./memoryViewer/render')
 const channelTitles = require('./channelTitles/render')
 
-exports.init = (state, dispatch) => {
-  const root = document.createElement('div')
-  root.classList.add('timeline-root')
-  root.id = 'timeline-root'
-
-  return root
+let observed = []
+const changed = (vals) => {
+  const allSame = vals.every((v, i) => v === observed[i])
+  observed = vals
+  return !allSame
 }
 
-exports.update = (state, dispatch) => {
-  // Remove old canvas
-  const root = document.getElementById('timeline-root')
-  if (root.firstChild) {
-    root.removeChild(root.firstChild)
-  }
-
-  // New canvas
+const renderCanvas = (state, dispatch) => {
   const canvas = document.createElement('div')
   canvas.classList.add('timeline-canvas')
 
@@ -28,5 +20,25 @@ exports.update = (state, dispatch) => {
   canvas.appendChild(channelTitles(state, dispatch))
   canvas.appendChild(memoryViewer(state, dispatch))
 
-  root.appendChild(canvas)
+  return canvas
+}
+
+exports.init = (state, dispatch) => {
+  const root = document.createElement('div')
+  root.classList.add('timeline-root')
+  root.id = 'timeline-root'
+
+  return root
+}
+
+exports.update = (state, dispatch) => {
+  if (changed([state.timeline, state.predictors.prediction])) {
+    // Remove old canvas
+    const root = document.getElementById('timeline-root')
+    if (root.firstChild) {
+      root.removeChild(root.firstChild)
+    }
+    const canvas = renderCanvas(state, dispatch)
+    root.appendChild(canvas)
+  }
 }
