6de75f2510fc9454df7f9fb0a991edefcd436f7a
Akseli Palen <akseli.palen@gmail.com>
2019-12-31 14:34:20 +0200

    Template for channelEditor

--- src/timeline/channelEditor/render.js
+++ src/timeline/channelEditor/render.js
@@ -1,3 +1,8 @@
+const template = require('./template.ejs')
+
+const listen = (el, query, eventName, handler) => {
+  el.querySelector(query).addEventListener(eventName, handler)
+}
 
 module.exports = (state, dispatch) => {
   const root = document.createElement('div')
@@ -7,77 +12,38 @@ module.exports = (state, dispatch) => {
   }
 
   const c = state.timeline.select.channel
-  const title = state.timeline.channels[c].title
 
-  root.classList.add('row')
-  const col = document.createElement('div')
-  col.classList.add('col-md')
-
-  const hel = document.createElement('h2')
-  hel.innerHTML = 'Channel'
-  col.appendChild(hel)
-
-  const form = document.createElement('form')
-
-  const text = document.createElement('input')
-  text.type = 'text'
-  text.value = title
-  form.appendChild(text)
-
-  const okBtn = document.createElement('button')
-  okBtn.type = 'button'
-  okBtn.innerHTML = 'OK'
-  form.appendChild(okBtn)
-
-  const newline = document.createElement('div')
-
-  const delBtn = document.createElement('button')
-  delBtn.type = 'button'
-  delBtn.innerHTML = 'DEL'
-  newline.appendChild(delBtn)
-
-  const leftBtn = document.createElement('button')
-  leftBtn.type = 'button'
-  leftBtn.innerHTML = 'Move left'
-  newline.appendChild(leftBtn)
-
-  const rightBtn = document.createElement('button')
-  rightBtn.type = 'button'
-  rightBtn.innerHTML = 'Move right'
-  newline.appendChild(rightBtn)
-
-  form.appendChild(newline)
-
-  col.appendChild(form)
-  root.appendChild(col)
+  root.innerHTML = template({
+    channelTitle: state.timeline.channels[c].title
+  })
 
   // Events
 
-  form.addEventListener('submit', ev => {
+  listen(root, '#channelForm', 'submit', ev => {
     ev.preventDefault()
     dispatch({
       type: 'EDIT_CHANNEL_TITLE',
       channel: c,
-      title: text.value
+      title: root.querySelector('#channelTitle').value
     })
   })
 
-  okBtn.addEventListener('click', ev => {
+  listen(root, '#channelTitleSubmit', 'click', ev => {
     dispatch({
       type: 'EDIT_CHANNEL_TITLE',
       channel: c,
-      title: text.value
+      title: root.querySelector('#channelTitle').value
     })
   })
 
-  delBtn.addEventListener('click', ev => {
+  listen(root, '#channelRemove', 'click', ev => {
     dispatch({
       type: 'REMOVE_CHANNEL',
       channel: c
     })
   })
 
-  leftBtn.addEventListener('click', ev => {
+  listen(root, '#channelMoveLeft', 'click', ev => {
     dispatch({
       type: 'MOVE_CHANNEL',
       channel: c,
@@ -85,7 +51,7 @@ module.exports = (state, dispatch) => {
     })
   })
 
-  rightBtn.addEventListener('click', ev => {
+  listen(root, '#channelMoveRight', 'click', ev => {
     dispatch({
       type: 'MOVE_CHANNEL',
       channel: c,
