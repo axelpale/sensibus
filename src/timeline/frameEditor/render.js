d41b923c0def05e8d5fe6eb8120628944ada7dac
Akseli Palen <akseli.palen@gmail.com>
2020-01-01 10:48:48 +0200

    Style frame form

--- src/timeline/frameEditor/render.js
+++ src/timeline/frameEditor/render.js
@@ -1,3 +1,8 @@
+const template = require('./template.ejs')
+
+const listen = (el, query, eventName, handler) => {
+  el.querySelector(query).addEventListener(eventName, handler)
+}
 
 module.exports = (state, dispatch) => {
   const root = document.createElement('div')
@@ -8,83 +13,30 @@ module.exports = (state, dispatch) => {
 
   const timeline = state.timeline
   const t = timeline.select.frame
-  const frameTitle = timeline.frames[t].title
 
-  root.classList.add('row')
-  const col = document.createElement('div')
-  col.classList.add('col-md')
-
-  const hel = document.createElement('h2')
-  hel.innerHTML = 'Frame'
-  col.appendChild(hel)
-
-  const form = document.createElement('form')
-
-  const text = document.createElement('input')
-  text.type = 'text'
-  text.value = frameTitle
-  form.appendChild(text)
-
-  const okBtn = document.createElement('button')
-  okBtn.type = 'button'
-  okBtn.innerHTML = 'OK'
-  form.appendChild(okBtn)
-
-  const delBtn = document.createElement('button')
-  delBtn.type = 'button'
-  delBtn.innerHTML = 'DEL'
-  form.appendChild(delBtn)
-
-  const upBtn = document.createElement('button')
-  upBtn.type = 'button'
-  upBtn.innerHTML = 'Move up'
-  form.appendChild(upBtn)
-
-  const downBtn = document.createElement('button')
-  downBtn.type = 'button'
-  downBtn.innerHTML = 'Move down'
-  form.appendChild(downBtn)
-
-  const breakBtn = document.createElement('button')
-  breakBtn.type = 'button'
-  breakBtn.innerHTML = 'Break below'
-  form.appendChild(breakBtn)
-
-  const delBreakBtn = document.createElement('button')
-  delBreakBtn.type = 'button'
-  delBreakBtn.innerHTML = 'Remove Break'
-  form.appendChild(delBreakBtn)
-
-  col.appendChild(form)
-  root.appendChild(col)
+  root.innerHTML = template({
+    frameTitle: state.timeline.frames[t].title
+  })
 
   // Events
 
-  form.addEventListener('submit', ev => {
+  listen(root, '#frameForm', 'submit', ev => {
     ev.preventDefault()
     dispatch({
       type: 'EDIT_FRAME_TITLE',
       frame: t,
-      title: text.value
+      title: root.querySelector('#frameTitle').value
     })
   })
 
-  okBtn.addEventListener('click', ev => {
-    dispatch({
-      type: 'EDIT_FRAME_TITLE',
-      frame: t,
-      title: text.value
-    })
-  })
-
-  delBtn.addEventListener('click', ev => {
+  listen(root, '#frameRemove', 'click', ev => {
     dispatch({
       type: 'REMOVE_FRAME',
       frame: t
     })
   })
 
-  upBtn.addEventListener('click', ev => {
+  listen(root, '#frameMoveUp', 'click', ev => {
     dispatch({
       type: 'MOVE_FRAME',
       frame: t,
@@ -92,7 +44,7 @@ module.exports = (state, dispatch) => {
     })
   })
 
-  downBtn.addEventListener('click', ev => {
+  listen(root, '#frameMoveDown', 'click', ev => {
     dispatch({
       type: 'MOVE_FRAME',
       frame: t,
@@ -100,14 +52,14 @@ module.exports = (state, dispatch) => {
     })
   })
 
-  breakBtn.addEventListener('click', ev => {
+  listen(root, '#frameCreateBreak', 'click', ev => {
     dispatch({
       type: 'CREATE_BREAK',
       beforeFrame: t
     })
   })
 
-  delBreakBtn.addEventListener('click', ev => {
+  listen(root, '#frameRemoveBreak', 'click', ev => {
     dispatch({
       type: 'REMOVE_BREAK',
       frame: t
