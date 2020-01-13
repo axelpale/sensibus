e438196cb2cc45c50cce481138e03c5ea62b0bcc
Akseli Palén <akseli.palen@gmail.com>
2020-01-13 12:19:40 +0200

    Explain copyPrevious

--- src/predictors/collection/copyPrevious/render.js
+++ src/predictors/collection/copyPrevious/render.js
@@ -1,4 +1,7 @@
 module.exports = (state, model, dispatch) => {
   const root = document.createElement('div')
+
+  root.innerHTML = '<p>Guess that value of the previous frame repeats</p>'
+
   return root
 }
