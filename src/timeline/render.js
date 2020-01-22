73a60ca1de010909eaba2c75c37d3dda0c95b2b6
Akseli Palen <akseli.palen@gmail.com>
2020-01-23 00:38:04 +0200

    Improve timeline positioning

--- src/timeline/render.js
+++ src/timeline/render.js
@@ -10,12 +10,21 @@ const changed = (vals) => {
   return !allSame
 }
 
+const convertRemToPx = (r) => {
+  const root = document.documentElement
+  return r * parseFloat(window.getComputedStyle(root).fontSize)
+}
+
 const renderCanvas = (state, dispatch) => {
   const canvas = document.createElement('div')
   canvas.classList.add('timeline-canvas')
 
   const W = way.width(state.timeline.memory)
-  canvas.style.width = '' + (W * 4.8 + 38).toFixed(2) + 'rem'
+  const viewportWidthPx = document.documentElement.clientWidth
+  const channelWidthPx = convertRemToPx(4.8)
+  const sidebarWidthPx = Math.min(viewportWidthPx * 0.9, convertRemToPx(30))
+  const canvasWidthPx = W * channelWidthPx + sidebarWidthPx
+  canvas.style.width = '' + (canvasWidthPx).toFixed(2) + 'px'
 
   canvas.appendChild(channelTitles(state, dispatch))
   canvas.appendChild(memoryViewer(state, dispatch))
