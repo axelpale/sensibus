4afd1d75a435ee8bed481a4b6ac401b3eeaaf2c2
Akseli Palén <akseli.palen@gmail.com>
2020-01-23 17:26:27 +0200

    Use problib

--- src/predictors/collection/filteredNaiveBayes/train/divergence.js
+++
@@ -1,6 +0,0 @@
-module.exports = (pxgy, px) => {
-  // Kullback-Leibler divergence Dkl(Pxgy||Px)
-  const pos = pxgy * Math.log2(pxgy / px)
-  const neg = (1 - pxgy) * Math.log2((1 - pxgy) / (1 - px))
-  return pos + neg
-}
