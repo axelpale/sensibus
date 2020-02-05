884045c63fc824a1483f483f3fcd2cc2209c5c41
Akseli Palén <akseli.palen@gmail.com>
2020-02-05 14:03:17 +0200

    Untested implementation of mRMR omega range search

--- src/predictors/collection/filteredNaiveBayes/train/findBestSubset/findBestIncrement.js
+++ src/predictors/collection/filteredNaiveBayes/train/findBestSubset/findBestIncrement.js
@@ -1,24 +1,44 @@
 const scoreIncrement = require('./scoreIncrement')
 const findIncrements = require('./findIncrements')
+const findSubsequences = require('./findSubsequences')
 
 module.exports = (priors, fields, miFields, slices, condChan) => {
-  // Default increment.
+  // Find mRMR increments
   const increments = findIncrements(miFields, condChan)
-  let bestScore = -1 // min MCC score
-  let bestAt = 0
 
+  // Score increments with MCC
   const scorings = increments.map((increment, i) => {
-    const scoring = scoreIncrement(priors, fields, slices, condChan, increment)
-    if (bestScore < scoring.score) {
-      bestScore = scoring.score
-      bestAt = i
+    return scoreIncrement(priors, fields, slices, condChan, increment)
+  })
+
+  // Find good and stable subsequence of scores.
+  const subseqs = findSubsequences(scorings)
+  let bestSubseqQuality = -1 // min MCC score
+  let bestSubseqAt = 0
+  subseqs.forEach((subseq, i) => {
+    if (bestSubseqQuality < subseq.quality) {
+      bestSubseqQuality = subseq.quality
+      bestSubseqAt = i
     }
-    return scoring
+  })
+  const bestSubseq = subseqs[bestSubseqAt]
+
+  // Find the best increment within the best subsequence.
+  const bestScorings = scorings.slice(bestSubseq.begin, bestSubseq.end)
+  let bestIncrementScore = bestScorings[0].score
+  let bestIncrementAt = bestSubseq.begin
+  bestScorings.forEach((scoring, j) => {
+    if (bestIncrementScore < scoring.score) {
+      bestIncrementScore = scoring.score
+      bestIncrementAt = bestSubseq.begin + j
+    }
   })
 
   return {
     increments: increments,
     scorings: scorings,
-    bestAt: bestAt
+    subsequences: subseqs,
+    bestSubseqAt: bestSubseqAt,
+    bestIncrementAt: bestIncrementAt
   }
 }
---
+++ src/predictors/collection/filteredNaiveBayes/train/findBestSubset/findSubsequences.js
@@ -0,0 +1,57 @@
+// mRMR omega range selector.
+// Find such stable subsequence in the sequence of feature increments
+// that is both high in validation score and low in score variance.
+
+module.exports = (scorings) => {
+  const subseqRecords = []
+
+  const seqSize = scorings.length
+  const subseqSize = Math.min(Math.ceil(seqSize / 5), 5)
+
+  // Subseq range begin and end
+  let a = 0 // inclusive
+  let b = subseqSize // exclusive
+
+  while (b <= seqSize) {
+    // Mean
+    let meanSum = 0
+    for (let i = 0; i < b - a; i += 1) {
+      meanSum += scorings[a + i].score
+    }
+    const subseqMean = meanSum / (b - a)
+
+    // Variance
+    let varSum = 0
+    for (let i = 0; i < b - a; i += 1) {
+      const diff = scorings[a + i].score - subseqMean
+      varSum += diff * diff
+    }
+    const subseqVar = varSum / (b - a)
+
+    // Feature set size penalty
+    const penalty = 0.0001 * a
+
+    // Score
+    const quality = subseqMean - subseqVar - penalty
+
+    // Record for inspection
+    subseqRecords.push({
+      begin: a,
+      end: b,
+      mean: subseqMean,
+      variance: subseqVar,
+      penalty: penalty,
+      quality: quality
+    })
+
+    // Next subsequence
+    a = b
+    b = b + subseqSize
+    // Extend the last subsequence to the end.
+    if (b + subseqSize > seqSize) {
+      b = seqSize
+    }
+  }
+
+  return subseqRecords
+}
