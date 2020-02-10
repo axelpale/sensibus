c665c447c60fc69d922a1c42ea6907784992d8fc
Akseli Palén <akseli.palen@gmail.com>
2020-02-11 01:53:23 +0200

    Working row-like channel editor

--- src/timeline/channelTitles/editorRow.js
+++ src/timeline/channelTitles/editorRow.js
@@ -1,15 +1,86 @@
 const template = require('./editorRow.ejs')
 
+const listen = (el, query, eventName, handler) => {
+  el.querySelector(query).addEventListener(eventName, handler)
+}
+
 module.exports = (state, dispatch) => {
   const editorRow = document.createElement('div')
-  editorRow.classList.add('timeline-row')
   editorRow.classList.add('channel-editor-row')
 
   // Assert select not null
-  const select = state.timeline.select
+  const c = state.timeline.select.channel
+
+  let buttonOrder
+  const numChannels = state.timeline.channels.length
+  if (c < 5) {
+    editorRow.style.left = '10rem'
+    buttonOrder = 'left'
+  } else if (c < numChannels - 7) {
+    editorRow.style.left = (c * 4.2 - 5) + 'rem'
+    buttonOrder = 'center'
+  } else {
+    editorRow.style.right = '12rem'
+    buttonOrder = 'right'
+  }
 
   editorRow.innerHTML = template({
-    chan: select.channel
+    chan: c,
+    channelTitle: state.timeline.channels[c].title,
+    buttonOrder: buttonOrder
+  })
+
+  // Events
+
+  listen(editorRow, '#channelForm', 'submit', ev => {
+    ev.preventDefault()
+    dispatch({
+      type: 'EDIT_CHANNEL_TITLE',
+      channel: c,
+      title: editorRow.querySelector('#channelTitle').value
+    })
+  })
+
+  listen(editorRow, '#channelRemove', 'click', ev => {
+    ev.preventDefault()
+    dispatch({
+      type: 'REMOVE_CHANNEL',
+      channel: c
+    })
+  })
+
+  listen(editorRow, '#channelMoveLeft', 'click', ev => {
+    ev.preventDefault()
+    dispatch({
+      type: 'MOVE_CHANNEL',
+      channel: c,
+      offset: -1
+    })
+  })
+
+  listen(editorRow, '#channelMoveRight', 'click', ev => {
+    ev.preventDefault()
+    dispatch({
+      type: 'MOVE_CHANNEL',
+      channel: c,
+      offset: 1
+    })
+  })
+
+  listen(editorRow, '#channelCreateLeft', 'click', ev => {
+    ev.preventDefault()
+    dispatch({
+      type: 'CREATE_CHANNEL',
+      belowChannel: c
+    })
+  })
+
+  listen(editorRow, '#channelCreateRight', 'click', ev => {
+    ev.preventDefault()
+    dispatch({
+      type: 'CREATE_CHANNEL',
+      belowChannel: c + 1
+    })
   })
 
   return editorRow
--- src/timeline/channelTitles/render.js
+++ src/timeline/channelTitles/render.js
@@ -3,11 +3,11 @@ const editorRow = require('./editorRow')
 require('./style.css')
 
 module.exports = (state, dispatch) => {
-  const root = document.createElement('div')
-  root.appendChild(titleRow(state, dispatch))
+  const root = titleRow(state, dispatch)
 
   const select = state.timeline.select
   if (select && select.frame === -1) {
+    root.classList.add('channel-editor-container')
     root.appendChild(editorRow(state, dispatch))
   }
 
