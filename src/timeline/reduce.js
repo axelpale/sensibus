cf995bb448d68eee88a0a9b2ecc09859355a0109
Akseli Palén <akseli.palen@gmail.com>
2020-02-12 10:53:29 +0200

    Merge channelTitles reducer to timeline reducer

--- src/timeline/reduce.js
+++ src/timeline/reduce.js
@@ -1,5 +1,4 @@
 const defaultTimeline = require('./defaultTimeline')
-const channelTitles = require('./channelTitles/reduce')
 const memoryViewer = require('./memoryViewer/reduce')
 const predictNextTitle = require('./predictNextTitle')
 const way = require('senseway')
@@ -12,7 +11,6 @@ module.exports = (state, ev) => {
     })
   }
 
-  state = channelTitles(state, ev)
   state = memoryViewer(state, ev)
 
   switch (ev.type) {
@@ -65,6 +63,65 @@ module.exports = (state, ev) => {
           select: L.select ? Object.assign({}, L.select, {
             frame: atT
           }) : null
+        })
+      })
+    }
+
+    case 'REMOVE_CHANNEL': {
+      const copy = state.timeline.channels.slice()
+      copy.splice(ev.channel, 1)
+
+      return Object.assign({}, state, {
+        timeline: Object.assign({}, state.timeline, {
+          memory: way.dropChannel(state.timeline.memory, ev.channel),
+          channels: copy,
+          select: {
+            channel: state.timeline.select.channel >= ev.channel
+              ? state.timeline.select.channel - 1
+              : state.timeline.select.channel,
+            frame: 0
+          }
+        })
+      })
+    }
+
+    case 'MOVE_CHANNEL': {
+      const channelsCopy = state.timeline.channels.slice()
+      const W = way.width(state.timeline.memory)
+      const source = ev.channel
+      const target = (W + ev.channel + ev.offset) % W
+
+      const removedChannelConf = channelsCopy[source]
+      channelsCopy.splice(source, 1)
+      channelsCopy.splice(target, 0, removedChannelConf)
+
+      const removedChannel = [state.timeline.memory[source]]
+      const afterDrop = way.dropChannel(state.timeline.memory, source)
+      const afterInsert = way.insertChannel(afterDrop, target, removedChannel)
+
+      return Object.assign({}, state, {
+        timeline: Object.assign({}, state.timeline, {
+          memory: afterInsert,
+          channels: channelsCopy,
+          select: {
+            channel: target,
+            frame: state.timeline.select.frame || 0
+          }
+        })
+      })
+    }
+
+    case 'EDIT_CHANNEL_TITLE': {
+      return Object.assign({}, state, {
+        timeline: Object.assign({}, state.timeline, {
+          channels: state.timeline.channels.map((chConf, c) => {
+            if (c === ev.channel) {
+              return Object.assign({}, chConf, {
+                title: ev.title
+              })
+            }
+            return chConf
+          })
         })
       })
     }
