dd2f9e93013baa3c5b6167c094c48b1007197717
unknown <jussi.kaatiala@gmail.com>
2020-02-17 16:36:07 +0200

    Api hello world

--- server/index.js
+++ server/index.js
@@ -4,4 +4,14 @@ const port = 8888
 
 app.use(express.static('client/'))
 
+app.get('/api/timeline', function (req, res) {
+
+    res.send('Hello GET');
+})
+
+app.post('/api/timeline', function (req, res) {
+
+    res.send('Hello POST');
+})
+
 app.listen(port, () => console.log(`Sensibus server listening on port ${port}`))
