bf1771b68507cbcce54844fbc313d27866c9762c
unknown <jussi.kaatiala@gmail.com>
2020-02-17 17:00:00 +0200

    Gitbash compatible npm scripts and fix lint issues

--- server/index.js
+++ server/index.js
@@ -5,13 +5,11 @@ const port = 8888
 app.use(express.static('client/'))
 
 app.get('/api/timeline', function (req, res) {
-
-    res.send('Hello GET');
+  res.send('Hello GET')
 })
 
 app.post('/api/timeline', function (req, res) {
-
-    res.send('Hello POST');
+  res.send('Hello POST')
 })
 
 app.listen(port, () => console.log(`Sensibus server listening on port ${port}`))
