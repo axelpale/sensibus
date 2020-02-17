326409f5b1e37e232b00d5e130673f4156c0db0a
unknown <jussi.kaatiala@gmail.com>
2020-02-17 17:38:05 +0200

    Database creation added

--- server/index.js
+++ server/index.js
@@ -1,6 +1,30 @@
 const express = require('express')
+const sqlite = require('sqlite3')
+const fs = require("fs")
+const path = require("path")
+
 const app = express()
 const port = 8888
+const dbdir = path.join(__dirname, 'db')
+const dbpath = path.join(dbdir, 'sensibus.db')
+
+if (!fs.existsSync(dbdir)){
+  fs.mkdirSync(dbdir)
+}
+
+const db = new sqlite.Database(dbpath, (err) => {
+  if (err) {
+    return console.error(err.message)
+  }
+
+  // add tables
+  const querystr = 'CREATE TABLE TIMELINE(dump TEXT, userid TEXT)'
+  db.run(querystr, (err) => {
+    if (err) {
+      return console.log(err.message)
+    }
+  })
+})
 
 app.use(express.static('client/'))
 
