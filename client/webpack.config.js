51676ee14cb7e4dbdd7921c82d8dc1384b12bff9
Akseli Palén <akseli.palen@gmail.com>
2020-02-25 18:28:32 +0200

    Build in production by default

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -25,6 +25,6 @@ module.exports = {
       }
     ],
   },
-  mode: 'development', // in {development, production}
+  mode: 'production', // in {development, production}
   devtool: 'source-map',
 }
