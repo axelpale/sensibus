e96e3540d46589001fc81a8171d2a3813253bb17
unknown <jussi.kaatiala@gmail.com>
2020-03-11 16:33:36 +0200

    Database service integration

--- server/api/handlers.js
+++ server/api/handlers.js
@@ -1,16 +1,34 @@
-exports.getOne = (req, res) => {
+const databaseService = require('database-service')
+
+exports.getOne = (req, res, next) => {
   const timelineId = req.params.timelineId
 
   // TODO validate timelineId
 
   // Fetch row from database
+  databaseService.getOneTimeline('fooman', (err, timelineStr) => {
+    if (err) {
+      return next(err)
+    }
 
-  // Return timeline to client
-  res.json({
-    id: timelineId
+    if (timelineStr === null) {
+      return res.status(404).send('Timeline not found')
+    }
+
+    // Return timeline to client
+    return res.json({
+      id: timelineId,
+      timeline: timelineStr
+    })
   })
 }
 
-exports.post = (req, res) => {
-  res.status(200).end()
+exports.create = (req, res, next) => {
+  databaseService.setOneTimeline('randomdummytimeline', 'fooman', (err) => {
+    if (err) {
+      return next(err)
+    }
+
+    res.status(200).end()
+  })
 }
--- server/api/routes.js
+++ server/api/routes.js
@@ -2,6 +2,6 @@ const handlers = require('./handlers')
 const router = require('express').Router()
 
 router.get('/timeline/:timelineId', handlers.getOne)
-router.post('/timeline', handlers.post)
+router.post('/timeline', handlers.create)
 
 module.exports = router
