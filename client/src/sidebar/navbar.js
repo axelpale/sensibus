3639d4d2fa384cdc3ac99b67160308dc74a85ffe
Akseli Palen <akseli.palen@gmail.com>
2020-04-03 00:20:28 +0300

    Sketchy refactor

---
+++ client/src/sidebar/navbar.js
@@ -0,0 +1,62 @@
+const template = require('./navbar.ejs')
+
+const listenId = (root, elemId, handler) => {
+  const el = root.querySelector('#' + elemId)
+  el.addEventListener('click', handler)
+}
+
+exports.create = (store, dispatch) => {
+  const sidebarPage = store.getState().sidebarPage
+
+  const root = document.createElement('div')
+  root.innerHTML = template({
+    sidebarPage: sidebarPage
+  })
+
+  listenId(root, 'navbar-brand', ev => {
+    ev.preventDefault()
+    dispatch({
+      type: 'CLOSE_SIDEBAR'
+    })
+  })
+
+  listenId(root, 'navbarView', ev => {
+    ev.preventDefault()
+    dispatch({
+      type: 'OPEN_PAGE',
+      page: 'view'
+    })
+  })
+  listenId(root, 'navbarInspect', ev => {
+    ev.preventDefault()
+    dispatch({
+      type: 'OPEN_PAGE',
+      page: 'inspect'
+    })
+  })
+  listenId(root, 'navbarPerformance', ev => {
+    ev.preventDefault()
+    dispatch({
+      type: 'OPEN_PAGE',
+      page: 'performance'
+    })
+  })
+  listenId(root, 'navbarStorage', ev => {
+    ev.preventDefault()
+    dispatch({
+      type: 'OPEN_PAGE',
+      page: 'storage'
+    })
+  })
+
+  return root
+}
+
+exports.updateTab = (store, dispatch) => {
+  const prevTab = document.querySelector('.sidebar .nav-item.active')
+  prevTab.classList.remove('active')
+
+  const sidebarPage = store.getState().sidebarPage
+  const nextTab = document.querySelector('.nav-item.page-' + sidebarPage)
+  nextTab.classList.add('active')
+}
--- client/src/sidebar/render.js
+++ client/src/sidebar/render.js
@@ -1,52 +1,110 @@
-const template = require('./template.ejs')
 require('./sidebar.css')
+const navbar = require('./navbar')
+const sidebarOpener = require('./sidebarOpener')
+const view = require('../view/render')
+const predictors = require('../predictors/render')
+const performance = require('../performance/render')
 
-const listenId = (root, elemId, handler) => {
-  const el = root.querySelector('#' + elemId)
-  el.addEventListener('click', handler)
+const sidebarChanged = createObserver([
+  state => state.sidebar
+])
+const sidebarPageChanged = createObserver([
+  state => state.sidebarPage
+])
+const performanceChanged = createObserver([
+  state => state.performance
+])
+
+const openSidebar = (store, dispatch) => {
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.classList.remove('active')
 }
 
-module.exports = (state, dispatch) => {
-  const root = document.createElement('div')
-  root.innerHTML = template({
-    sidebarPage: state.sidebarPage
-  })
+const closeSidebar = (store, dispatch) => {
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.classList.add('d-none')
+}
 
-  listenId(root, 'navbar-brand', ev => {
-    ev.preventDefault()
-    dispatch({
-      type: 'CLOSE_SIDEBAR'
-    })
-  })
+const createPage = (store, dispatch) => {
+  // Returns pageEl for sidebarPage name.
+  const sidebarPage = store.getState().sidebarPage
+  switch (sidebarPage) {
+    case 'inspect':
+      return predictors.create(store, dispatch)
 
-  listenId(root, 'navbarView', ev => {
-    ev.preventDefault()
-    dispatch({
-      type: 'OPEN_PAGE',
-      page: 'view'
-    })
-  })
-  listenId(root, 'navbarInspect', ev => {
-    ev.preventDefault()
-    dispatch({
-      type: 'OPEN_PAGE',
-      page: 'inspect'
-    })
-  })
-  listenId(root, 'navbarPerformance', ev => {
-    ev.preventDefault()
-    dispatch({
-      type: 'OPEN_PAGE',
-      page: 'performance'
-    })
-  })
-  listenId(root, 'navbarStorage', ev => {
-    ev.preventDefault()
-    dispatch({
-      type: 'OPEN_PAGE',
-      page: 'storage'
-    })
-  })
+    case 'view':
+      return view.create(store, dispatch)
 
-  return root
+    case 'performance':
+      return performance.create(store, dispatch)
+
+    case 'storage':
+      return storage.create(store, dispatch)
+
+    default:
+      return null
+  }
+}
+
+exports.create = (store, dispatch) => {
+  const sidebarContainer = document.createElement('div')
+  sidebarContainer.classList.add('sidebar-container')
+  sidebarContainer.id = 'sidebarContainer'
+
+  const sidebar = document.createElement('div')
+  sidebar.classList.add('sidebar')
+  sidebarContainer.appendChild(sidebar)
+
+  const navbarEl = navbar.create(store, dispatch)
+  sidebar.appendChild(navbarEl)
+
+  const contentContainer = document.createElement('div')
+  contentContainer.classList.add('sidebar-content')
+  contentContainer.classList.add('container-fluid')
+  sidebar.appendChild(contentContainer)
+
+  sidebarContainer.appendChild(sidebarOpener.create(store, dispatch))
+}
+
+exports.update = (store, dispatch) => {
+  const state = store.getState()
+  const isSidebarChanged = sidebarChanged(state)
+  const isSidebarPageChanged = sidebarPageChanged(state)
+  const isTimelineChanged = timelineChanged(state)
+  const isPerfChanged = performanceChanged(state)
+
+  // Open/close sidebar and sidebar opener
+  if (isSidebarChanged) {
+    if (state.sidebar) {
+      // Sidebar open
+      sidebarOpener.hide(store, dispatch)
+      openSidebar(store, dispatch)
+    } else {
+      // Sidebar closed
+      sidebarOpener.show(state, dispatch)
+      closeSidebar(store, dispatch)
+    }
+  }
+
+  // Create pages when sidebar opens or page is changed.
+  if (state.sidebar && (isSidebarPageChanged || isSidebarChanged)) {
+    // Update navbar on page change and page open.
+    navbar.updateTab(store, dispatch)
+
+    // First, clear content container (cc)
+    const cc = document.querySelector('.sidebar-content')
+    if (cc.firstChild) {
+      cc.removeChild(cc.firstChild)
+    }
+
+    // Then, render the page.
+    cc.appendChild(createPage(store, dispatch))
+  }
+
+  // Update performance
+  const isPerfPage = state.sidebarPage === 'performance'
+  if (state.sidebar && isPerfPage && isPerfChanged) {
+    performance.update(store, dispatch)
+  }
+
 }
