5c9162480c9654664f058927e1e5edf93914a838
Akseli Palén <akseli.palen@gmail.com>
2020-04-04 17:46:03 +0300

    Move all sidebar stuff under sidebar

--- client/src/render.js
+++ client/src/render.js
@@ -1,33 +1,13 @@
 require('./style.css')
-const sidebarView = require('./sidebar/render')
-const storage = require('./storage/render')
+const sidebar = require('./sidebar/render')
 const timeline = require('./timeline/render')
-const view = require('./view/render')
-const predictors = require('./predictors/render')
-const performance = require('./performance/render')
-const sidebarOpener = require('./sidebar/sidebarOpener')
 const createObserver = require('uilib').createObserver
 
+// TODO move under timeline/render
 const timelineChanged = createObserver([
   state => state.timeline,
   state => state.predictors.prediction
 ])
-const sidebarChanged = createObserver([
-  state => state.sidebar
-])
-const sidebarPageChanged = createObserver([
-  state => state.sidebarPage
-])
-const performanceChanged = createObserver([
-  state => state.performance
-])
-// const selectChanged = createObserver([
-//   state => state.timeline.select
-// ])
-
-let sidebar
-let sidebarContainer
-let contentContainer
 
 exports.init = (store, dispatch) => {
   const container = document.getElementById('container')
@@ -41,10 +21,7 @@ exports.init = (store, dispatch) => {
   root.appendChild(timeline.create(store, dispatch))
 
   // Sidebar
-  sidebarContainer = document.createElement('div')
-  sidebarContainer.classList.add('sidebar-container')
-  sidebarContainer.id = 'sidebarContainer'
-  root.appendChild(sidebarContainer)
+  root.appendChild(sidebar.create(store, dispatch))
 
   // Add to DOM
   container.appendChild(root)
@@ -52,75 +29,10 @@ exports.init = (store, dispatch) => {
 
 exports.update = (store, dispatch) => {
   const state = store.getState()
-  // const isSelectChanged = selectChanged(state)
-  const isTimelineChanged = timelineChanged(state)
-  const isSidebarChanged = sidebarChanged(state)
-  const isSidebarPageChanged = sidebarPageChanged(state)
 
-  if (isTimelineChanged) {
+  if (timelineChanged(state)) { // TODO move condition in timeline
     timeline.update(store, dispatch)
   }
 
-  if (isSidebarChanged) {
-    // Clear element.
-    if (sidebarContainer.firstChild) {
-      sidebarContainer.removeChild(sidebarContainer.firstChild)
-    }
-
-    if (state.sidebar) {
-      sidebar = document.createElement('div')
-      sidebar.classList.add('sidebar')
-      sidebar.appendChild(sidebarView(state, dispatch))
-
-      contentContainer = document.createElement('div')
-      contentContainer.classList.add('sidebar-content')
-      contentContainer.classList.add('container-fluid')
-
-      sidebar.appendChild(contentContainer)
-      sidebarContainer.appendChild(sidebar)
-    } else {
-      // Sidebar closed
-      sidebarContainer.appendChild(sidebarOpener(state, dispatch))
-    }
-  }
-
-  // Run page updates
-  if (state.sidebar) {
-    // First, clear content container
-    if (contentContainer.firstChild) {
-      contentContainer.removeChild(contentContainer.firstChild)
-    }
-
-    // TODO create and update for each page
-    switch (state.sidebarPage) {
-      case 'inspect':
-        contentContainer.appendChild(predictors(state, dispatch))
-        break
-
-      case 'view':
-        contentContainer.appendChild(view(store, dispatch))
-        break
-
-      case 'performance':
-        contentContainer.appendChild(performance(state, dispatch))
-        break
-
-      case 'storage':
-        contentContainer.appendChild(storage(state, dispatch))
-        break
-
-      default:
-        break
-    }
-  }
-
-  if (state.sidebar && (isSidebarPageChanged || isSidebarChanged)) {
-    // Re-render sidebar when opened, closed or somehow changed
-    sidebar.replaceChild(sidebarView(state, dispatch), sidebar.firstChild)
-  }
-
-  const isPerfPage = state.sidebarPage === 'performance'
-  if (state.sidebar && isPerfPage && performanceChanged(state)) {
-    performance(state, dispatch)
-  }
+  sidebar.update(store, dispatch)
 }
--- client/src/sidebar/sidebarOpener.js
+++ client/src/sidebar/sidebarOpener.js
@@ -1,8 +1,13 @@
-module.exports = (state, dispatch) => {
+exports.create = (store, dispatch) => {
   // Sidebar is closed. Render opener.
   const opener = document.createElement('div')
   opener.classList.add('sidebar-opener')
   opener.classList.add('bg-dark')
+
+  if (store.getState().sidebar === false) {
+    opener.classList.add('d-none')
+  }
+
   const openerIcon = document.createElement('img')
   openerIcon.src = 'img/icon.png'
   openerIcon.width = 30
@@ -16,4 +21,14 @@ module.exports = (state, dispatch) => {
   })
 
   return opener
+}
+
+exports.hide = (store, dispatch) => {
+  const opener = document.querySelector('.sidebar-opener')
+  opener.classList.add('d-none')
+}
+
+exports.show = (store, dispatch) => {
+  const opener = document.querySelector('.sidebar-opener')
+  opener.classList.remove('d-none')
 }
