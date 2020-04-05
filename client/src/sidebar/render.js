532538a4d238f1494a670aec5593133af17e597d
Akseli Palen <akseli.palen@gmail.com>
2020-04-05 21:08:11 +0300

    Fix sidebar bug

--- client/src/sidebar/render.js
+++ client/src/sidebar/render.js
@@ -19,7 +19,7 @@ const performanceChanged = createObserver([
 
 const openSidebar = (store, dispatch) => {
   const sidebar = document.querySelector('.sidebar')
-  sidebar.classList.remove('active')
+  sidebar.classList.remove('d-none')
 }
 
 const closeSidebar = (store, dispatch) => {
