b2cd098082deaa533bbf2b128fe55895d310dc89
Akseli Palen <akseli.palen@gmail.com>
2020-04-10 14:23:59 +0300

    Route static files of timeline client

--- server/timeline/routes.js
+++ server/timeline/routes.js
@@ -1,6 +1,11 @@
-const router = require('express').Router()
+const express = require('express')
+const router = express.Router()
 const handlers = require('./handlers')
+const path = require('path')
 
+const timelineClientPath = path.resolve(__dirname, '../../timeline')
+
+router.use('/:timelineId', express.static(timelineClientPath))
 router.get('/:timelineId', handlers.render)
 
 module.exports = router
