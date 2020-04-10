d6211a6e1734900bb2b76725bd5bbce0b58b5587
Akseli Palen <akseli.palen@gmail.com>
2020-04-10 14:23:43 +0300

    Create own package.json for timeline client

--- timeline/webpack.config.js
+++ timeline/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './timeline/src/index.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
