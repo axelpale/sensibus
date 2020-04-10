87ad9d21aa2c9f081e1541fe9f0cd28ea692fa74
juissi999 <jussi.kaatiala@gmail.com>
2020-04-10 12:58:29 +0300

    Added bootstrap to react client

--- client/App.jsx
+++ client/App.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import LandingPage from './components/LandingPage.jsx'
 import NotFoundPage from './components/NotFoundPage.jsx'
 import TimelinePage from './components/TimelinePage.jsx'
