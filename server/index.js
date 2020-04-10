c34d39a2f328a9c77843d45f590b5b4df452e152
Akseli Palen <akseli.palen@gmail.com>
2020-04-10 14:23:17 +0300

    Response with site client by default

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const path = require('path')
 const router = require('./routes')
 
 const app = express()
@@ -8,5 +8,11 @@ const port = 8888
 app.use(express.static('site/dist/'))
 
 app.use('/', router)
+
+// return index for all the other routes (which are used and
+// processed in react router in the app)
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../site/dist/index.html'))
+})
 
 app.listen(port, () => console.log(`Sensibus server listening on port ${port}`))
