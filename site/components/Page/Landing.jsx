567304f4458585078e878c3fb242867868d61e30
juissi999 <jussi.kaatiala@gmail.com>
2020-04-22 18:29:56 +0300

    Popular and new timelines site-side

--- site/components/Page/Landing.jsx
+++ site/components/Page/Landing.jsx
@@ -1,18 +1,44 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Page from './index.jsx'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
 import CreateTimelineForm from '../CreateTimelineForm.jsx'
+import apiService from '../../apiService.js'
 
 const LandingPage = () => {
+
+  const [popular, setPopular] = useState([])
+  const [recent, setRecent] = useState([])
+
+  useEffect(() => {
+    apiService
+      .getRecentTimelines()
+      .then(data=>{setRecent(data)})
+
+    apiService
+      .getPopularTimelines()
+      .then(data=>{setPopular(data)})
+  }, [])
   
+  const mapThings = (things) => {
+    // el.id el.title
+    return things.map((el, i)=>{return (<div key={i}><a href={`/t/${el.id}`} >{el.title}</a></div>)})
+  }
+
   return (<Page>
             <h1>Sensibus.io</h1>
             <CreateTimelineForm/>
-            <br/>
-            or see popular timelines
-            <br/>
-            or see new timelines
-            <br/>
-            two columns
+            <Row className='mt-3'>
+              <Col>
+                <h3>Popular timelines</h3>
+                {mapThings(popular)}
+              </Col>
+              <Col>
+                <h3>New timelines</h3>
+                {mapThings(recent)}
+              </Col>
+            </Row>
           </Page>)
 }
 
