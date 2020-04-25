73bb6a2d8eece778b440acc16059ce361d45a731
juissi999 <jussi.kaatiala@gmail.com>
2020-04-25 21:45:41 +0300

    Footer added

--- site/components/Page/index.jsx
+++ site/components/Page/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-
+import Footer from '../Footer.jsx'
 
 const Page = ({children}) => {
   return (<div className='site'>
@@ -10,6 +10,7 @@ const Page = ({children}) => {
               <Row>
                 <Col>{children}</Col>
               </Row>
+              <Footer/>
             </Container>
           </div>)
 }
