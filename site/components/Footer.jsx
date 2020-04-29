0cb0d37af5ed2a3463406635db4a132b5e9470e4
Akseli Palen <akseli.palen@gmail.com>
2020-04-30 00:09:16 +0300

    Fix lint issue

--- site/components/Footer.jsx
+++ site/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
-import styles from './footer.css'
+import './footer.css'
 
 const Footer = () => {
   return (
