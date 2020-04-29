9c43edc54cc9ec04e401dc2752f532071840051b
juissi999 <jussi.kaatiala@gmail.com>
2020-04-29 21:12:12 +0300

    Signup page sketch

--- site/apiService.js
+++ site/apiService.js
@@ -1,14 +1,19 @@
 import axios from 'axios'
-const baseUrl = '/api/timeline'
+const baseUrl = '/api'
 
 const getRecentTimelines = () => {
-  const promise = axios.get(`${baseUrl}?sort=recent`)
+  const promise = axios.get(`${baseUrl}/timeline?sort=recent`)
   return promise.then(response => response.data)
 }
 
 const getPopularTimelines = () => {
-  const promise = axios.get(`${baseUrl}?sort=popular`)
+  const promise = axios.get(`${baseUrl}/timeline?sort=popular`)
   return promise.then(response => response.data)
 }
 
-export default { getRecentTimelines, getPopularTimelines }
+const postAccount = () => {
+  const promise = axios.post(`${baseUrl}/account/`)
+  return promise.then(response => response.data)
+}
+
+export default { getRecentTimelines, getPopularTimelines, postAccount }
--- site/components/Page/SignUp.jsx
+++ site/components/Page/SignUp.jsx
@@ -1,13 +1,43 @@
 import React from 'react'
 import Page from './index.jsx'
 import { Link } from 'react-router-dom'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import apiService from '../../apiService.js'
 
 const SignUpPage = () => {
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+    const email = document.getElementById('formBasicEmail').value
+    const pwd = document.getElementById('formBasicPassword').value
+    const user = document.getElementById('formBasicUser').value
+
+    const account = {username:user, email:email, password:pwd}
+    apiService.postAccount(account)
+  }
+
   return (
     <Page>
       <h1>Sign up</h1>
-      Hello.
-      <br />
+      <Form action='#' method='post' onSubmit={onSubmit}>
+        <Form.Group controlId='formBasicEmail'>
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type='email' placeholder='Enter email' />
+        </Form.Group>
+        <Form.Group controlId='formBasicPassword'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control type='password' placeholder='Password' />
+        </Form.Group>
+        <Form.Group controlId='formBasicUser'>
+          <Form.Label>Username</Form.Label>
+          <Form.Control type='text' placeholder='Username' />
+        </Form.Group>
+        <Button variant='primary' type='submit'>
+          Submit
+        </Button>
+      </Form>
+      
       <Link to='/'>main page</Link>
     </Page>
   )
