8daedf66530b9bd2f64f0cb4e6885e4f5360ac15
juissi999 <jussi.kaatiala@gmail.com>
2020-05-06 23:50:53 +0300

    UserPage to have a raw response for api-request

--- site/components/Page/User.jsx
+++ site/components/Page/User.jsx
@@ -1,23 +1,44 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Page from './index.jsx'
 import { useParams } from 'react-router-dom'
 import sensibusApi from 'sensibus-api-client'
 
 const UserPage = () => {
-  let { userId } = useParams()
+  const { userId } = useParams()
+
+  const [userDetails, setUserDetails] = useState(null)
 
   useEffect(() => {
     sensibusApi
       .getUser(userId)
-      .then((value) => console.log(value))
+      .then((value) => setUserDetails(value))
   }, [])
 
-  return (
+  if (userDetails !== null) {
+    if (Object.keys(userDetails).length === 0) {
+      // user was gotten but was empty -> not found
+      return (
+        <Page>
+          <h1>User not found</h1>
+          User {userId} does not exist
+        </Page>
+      )
+    } else {
+      return (
+        <Page>
+          <h1>{userId}</h1>
+          {userDetails}
+        </Page>
+    )  
+    }
+  } else {
+    return (
       <Page>
         <h1>{userId}</h1>
-        user information here
+        Retrieving information...
       </Page>
   )
+  }
 }
 
 export default UserPage
