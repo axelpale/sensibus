e01ff7de78f3dbd32d5655abe928781330e44b7a
Akseli Palen <akseli.palen@gmail.com>
2020-05-06 17:59:45 +0300

    Restyle sidebar opener as Stats for Nerds

--- timeline/src/sidebar/sidebarOpener.js
+++ timeline/src/sidebar/sidebarOpener.js
@@ -2,16 +2,13 @@ exports.create = (store, dispatch) => {
   // Sidebar is closed. Render opener.
   const opener = document.createElement('div')
   opener.classList.add('sidebar-opener')
-  opener.classList.add('bg-dark')
 
   if (store.getState().sidebar === false) {
     opener.classList.add('d-none')
   }
 
-  const openerIcon = document.createElement('img')
-  openerIcon.src = 'img/icon.png'
-  openerIcon.width = 30
-  openerIcon.height = 30
+  const openerIcon = document.createElement('span')
+  openerIcon.innerHTML = 'Stats for Nerds'
   opener.appendChild(openerIcon)
 
   opener.addEventListener('click', ev => {
