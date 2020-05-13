5936281e1a21004b829c88caaddab4fa6c4d4f76
juissi999 <jussi.kaatiala@gmail.com>
2020-05-13 19:51:53 +0300

    CreateTimeline-button to work with updated api

--- site/components/Form/CreateTimeline.jsx
+++ site/components/Form/CreateTimeline.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import sensibusApi from 'sensibus-api-client'
 
 const CreateTimelineForm = () => {
+
+  const onClick = () => {
+    sensibusApi
+      .postTimeline()
+      .then((obj) => window.location.replace(`/t/${obj.timelineId}`) )
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
   return (
-    <Form action='/t' method='post'>
-      <Button variant='primary' type='submit'>
+    <Form>
+      <Button variant='primary' onClick={onClick}>
         Create new timeline
       </Button>
     </Form>
