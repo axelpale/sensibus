c0cbeaab6fafdd6ef5bf702d85b33fb7a5975492
juissi999 <jussi.kaatiala@gmail.com>
2020-05-22 19:18:55 +0300

    Add raw states to LoginBar

--- site/components/Bar/Login.jsx
+++ site/components/Bar/Login.jsx
@@ -3,55 +3,73 @@ import Button from 'react-bootstrap/Button'
 import Navbar from 'react-bootstrap/Navbar'
 import Form from 'react-bootstrap/Form'
 import FormControl from 'react-bootstrap/FormControl'
+import sensibusApi from 'sensibus-api-client'
 
 const LoginBar = ({}) => {
-  const [loginFormVisible, setLoginFormVisible] = useState(false)
-  const [userDetails, setUserDetails] = useState({ userName: '', passwd: '' })
+  // states:
+  // 0 - show loginbutton
+  // 1 - show loginform
+  // 2 - show when waiting for response
+  // 3 - show when error (timeout/button to state 0)
+  // 4 - show logged in details
+
+  const [loginState, setLoginState] = useState(0)
+  const [userDetails, setUserDetails] = useState({ email: '', password: '' })
 
   const onClickShowLogin = () => {
-    setLoginFormVisible(!loginFormVisible)
+    setLoginState(1)
   }
 
   const onClickSendLogin = event => {
-    console.log(userDetails)
+    setLoginState(2)
+    sensibusApi
+      .postLogin(userDetails)
+      .then(() => setLoginState(4))
+      .catch(() => setLoginState(3))
   }
 
-  if (loginFormVisible) {
-    return (
-      <Navbar bg='dark' variant='dark'>
-        <Navbar.Collapse className='justify-content-end'>
-          <Form inline>
-            <FormControl
-              type='text'
-              placeholder='Username'
-              className='mr-sm-2'
-              onChange={e =>
-                setUserDetails({ ...userDetails, userName: e.target.value })
-              }
-            />
-            <FormControl
-              type='password'
-              placeholder='Password'
-              className='mr-sm-2'
-              onChange={e =>
-                setUserDetails({ ...userDetails, passwd: e.target.value })
-              }
-            />
-            <Button variant='outline-info' onClick={onClickSendLogin}>
-              Login
-            </Button>
-          </Form>
-        </Navbar.Collapse>
-      </Navbar>
-    )
+  const renderLoginForm = state => {
+    if (state == 0) {
+      return (
+        <Button variant='outline-info' onClick={onClickShowLogin}>
+          LOGIN
+        </Button>
+      )
+    } else if (state == 1) {
+      return (
+        <Form inline>
+          <FormControl
+            type='text'
+            placeholder='Username/email'
+            className='mr-sm-2'
+            onChange={e =>
+              setUserDetails({ ...userDetails, email: e.target.value })
+            }
+          />
+          <FormControl
+            type='password'
+            placeholder='Password'
+            className='mr-sm-2'
+            onChange={e =>
+              setUserDetails({ ...userDetails, password: e.target.value })
+            }
+          />
+          <Button variant='outline-info' onClick={onClickSendLogin}>
+            Login
+          </Button>
+        </Form>
+      )
+    } else if (state == 2) {
+      return <Navbar.Text>Logging in...</Navbar.Text>
+    } else if (state == 3) {
+      return <Navbar.Text>Error</Navbar.Text>
+    } else if (state == 4) return <Navbar.Text> logged in</Navbar.Text>
   }
 
   return (
     <Navbar bg='dark' variant='dark'>
       <Navbar.Collapse className='justify-content-end'>
-        <Button variant='outline-info' onClick={onClickShowLogin}>
-          LOGIN
-        </Button>
+        {renderLoginForm(loginState)}
       </Navbar.Collapse>
     </Navbar>
   )
