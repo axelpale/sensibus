d3389c02c9b5be11de2554c78403f6325a1dc5d2
Akseli Palen <akseli.palen@gmail.com>
2020-05-25 11:29:02 +0300

    Use the persistence layer to access backend

--- timeline/src/index.js
+++ timeline/src/index.js
@@ -2,26 +2,10 @@ const redux = require('redux')
 const reducer = require('./reduce')
 const renderer = require('./render')
 const hibernator = require('./hibernate')
-const migrate = require('./migrate')
+const persistence = require('./persistence')
 
-// Get timeline id
-const timelineId = window.sensibus.timelineId
-
-// Hydrate from localStorage
-const storageName = 'sensibus-state'
-const storedState = window.localStorage.getItem(storageName)
-let initialState
-if (storedState) {
-  const readState = JSON.parse(storedState)
-  initialState = migrate(readState)
-} else {
-  initialState = null
-}
-
-const hibernate = (state) => {
-  const stateJson = JSON.stringify(state)
-  window.localStorage.setItem(storageName, stateJson)
-}
+// Get state if stored in browser.
+const initialState = persistence.getLocalState()
 
 // Init store
 const store = redux.createStore(reducer, initialState)
@@ -30,10 +14,25 @@ const dispatch = ev => store.dispatch(ev)
 // Render
 store.subscribe(() => {
   renderer.update(store, dispatch)
-  const state = store.getState()
-  hibernate(hibernator(state))
+  persistence.queueSaveState(store, (err, msg) => {
+    if (err) {
+      console.error(err)
+    } else {
+      console.log('Timeline saved.', msg)
+    }
+  })
 })
 
 renderer.init(store, dispatch)
 
 dispatch({ type: '__INIT__' })
+
+// Fetch possibly updated state from server
+persistence.getGlobalState((err, rawState) => {
+  dispatch({
+    type: 'SET_TIMELINE',
+    channels: rawState.channels,
+    frames: rawState.frames,
+    memory: rawState.memory
+  })
+})
