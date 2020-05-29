32438df3b3dbe48f71b925ad33a40b0fde61e58b
juissi999 <jussi.kaatiala@gmail.com>
2020-05-29 17:37:30 +0300

    Refactor LoginForm to a distinct component

--- site/components/Bar/Login.jsx
+++ site/components/Bar/Login.jsx
@@ -1,12 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import Button from 'react-bootstrap/Button'
 import Navbar from 'react-bootstrap/Navbar'
-import Form from 'react-bootstrap/Form'
-import FormControl from 'react-bootstrap/FormControl'
 import SignUpForm from '../Form/SignUp.jsx'
-import sensibusApi from 'sensibus-api-client'
 import sensibusToken from 'sensibus-token'
-//import jwtDecode from 'jwt-decode'
+import LoginForm from '../Form/Login.jsx'
 
 const LoginBar = ({}) => {
   // states:
@@ -17,13 +14,11 @@ const LoginBar = ({}) => {
   // 4 - show logged in details
 
   const [loginState, setLoginState] = useState(0)
-  const [loginDetails, setLoginDetails] = useState({ email: '', password: '' })
   const [loggedInUserDetails, setLoggedInUserDetails] = useState({})
 
   // at the start watch if token is in local storage
   useEffect(() => {
-    const lsToken = sensibusToken.getToken() //window.localStorage.getItem('sensibusToken')
-    if (lsToken !== null) {
+    if (sensibusToken.hasToken()) {
       setLoggedInUserDetails(sensibusToken.getDecoded())
       setLoginState(4)
     }
@@ -34,24 +29,8 @@ const LoginBar = ({}) => {
     setLoginState(1)
   }
 
-  const onClickSendLogin = event => {
-    setLoginState(2)
-    sensibusApi
-      .postLogin(loginDetails)
-      .then(token => {
-        sensibusToken.setToken(token)
-        //window.localStorage.setItem('sensibusToken', token)
-        setLoginState(4)
-      })
-      .catch(err => {
-        console.log(err)
-        setLoginState(3)
-      })
-  }
-
   const onClickLogout = () => {
     // remove token from local storage
-    // window.localStorage.removeItem('sensibusToken')
     sensibusToken.removeToken()
     setLoginState(0)
   }
@@ -67,29 +46,7 @@ const LoginBar = ({}) => {
         </>
       )
     } else if (state == 1) {
-      return (
-        <Form inline onSubmit={onClickSendLogin}>
-          <FormControl
-            type='text'
-            placeholder='Username/email'
-            className='mr-sm-2'
-            onChange={e =>
-              setLoginDetails({ ...loginDetails, email: e.target.value })
-            }
-          />
-          <FormControl
-            type='password'
-            placeholder='Password'
-            className='mr-sm-2'
-            onChange={e =>
-              setLoginDetails({ ...loginDetails, password: e.target.value })
-            }
-          />
-          <Button variant='outline-info' type='submit'>
-            Login
-          </Button>
-        </Form>
-      )
+      return <LoginForm setLoginState={setLoginState} />
     } else if (state == 2) {
       return <Navbar.Text>Logging in...</Navbar.Text>
     } else if (state == 3) {
---
+++ site/components/Form/Login.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import sensibusApi from 'sensibus-api-client'
+import sensibusToken from 'sensibus-token'
+
+const LoginForm = ({ setLoginState }) => {
+  const [loginDetails, setLoginDetails] = useState({ email: '', password: '' })
+
+  const onClickSendLogin = event => {
+    setLoginState(2)
+    sensibusApi
+      .postLogin(loginDetails)
+      .then(token => {
+        sensibusToken.setToken(token)
+        setLoginState(4)
+      })
+      .catch(err => {
+        setLoginState(3)
+      })
+  }
+
+  return (
+    <Form inline onSubmit={onClickSendLogin}>
+      <FormControl
+        type='text'
+        placeholder='Username/email'
+        className='mr-sm-2'
+        onChange={e =>
+          setLoginDetails({ ...loginDetails, email: e.target.value })
+        }
+      />
+      <FormControl
+        type='password'
+        placeholder='Password'
+        className='mr-sm-2'
+        onChange={e =>
+          setLoginDetails({ ...loginDetails, password: e.target.value })
+        }
+      />
+      <Button variant='outline-info' type='submit'>
+        Login
+      </Button>
+    </Form>
+  )
+}
+
+export default LoginForm
