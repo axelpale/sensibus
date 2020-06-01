49bf8d7899e2c2942ed5a5d1a7fafda2f2bfcc47
juissi999 <jussi.kaatiala@gmail.com>
2020-06-01 21:24:40 +0300

    Create timeline button forward to signup

--- site/components/Form/CreateTimeline.jsx
+++ site/components/Form/CreateTimeline.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import sensibusApi from 'sensibus-api-client'
+import sensibusToken from 'sensibus-token'
 
 const CreateTimelineForm = () => {
+  const history = useHistory()
+
   const onClick = () => {
-    sensibusApi
-      .postTimeline()
-      .then(obj => window.location.replace(`/t/${obj.timelineId}`))
-      .catch(err => {
-        console.log(err)
-      })
+    if (sensibusToken.hasToken()) {
+      sensibusApi
+        .postTimeline()
+        .then(obj => window.location.replace(`/t/${obj.timelineId}`))
+        .catch(err => {
+          console.log(err)
+        })
+    } else {
+      history.push('/signup')
+    }
   }
 
   return (
