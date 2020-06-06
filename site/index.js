9b27c97d5949bdaa1bfea172c98181f273192006
juissi999 <jussi.kaatiala@gmail.com>
2020-06-06 21:53:12 +0300

    Add login-reducer sketch

--- site/index.js
+++ site/index.js
@@ -1,5 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.jsx'
+import { createStore } from 'redux'
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+const loginReducer = (state = false, action) => {
+  // redux reducer for login state
+  switch (action.type) {
+    case 'LOGIN':
+      return true
+    case 'LOGOUT':
+      return false
+    default:
+      return state
+  }
+}
+
+const store = createStore(loginReducer)
+
+const renderApp = () =>
+  ReactDOM.render(<App store={store} />, document.querySelector('#root'))
+
+renderApp()
+store.subscribe(renderApp)
